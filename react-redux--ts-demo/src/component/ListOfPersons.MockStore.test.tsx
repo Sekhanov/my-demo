503d1a53352a746a9211e596b8c9ac46c50fdf2c
sergey.khanov <sergey.khanov@akvelon.com>
2019-12-26 13:31:41 +0300

    enzyme test

--- react-redux--ts-demo/src/component/ListOfPersons.MockStore.test.tsx
+++ react-redux--ts-demo/src/component/ListOfPersons.MockStore.test.tsx
@@ -1,33 +1,16 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { allReducers } from '../Reducers';
-import ListOfPersonsWithoutRedux from './ListOfPersonsWithoutRedux';
-import { persons, mockStoreData } from '../mock/mockData';
-import { ListOfPersons } from './ListOfPersons';
-import configureStore from 'redux-mock-store'
+import configureStore from 'redux-mock-store';
 import App from '../App';
-
-
+import { mockStoreData } from '../mock/mockData';
+import { ListOfPersons } from './ListOfPersons';
 
 
 const mockStoreCreator = configureStore();
-const initialState = { output: 100 };
 let mockStore = mockStoreCreator(mockStoreData);
 
-
-
-// it('react test library', () => {
-//     const { getByText } = render(
-//         <ListOfPersonsWithoutRedux
-//             persons={persons}
-//             someString={'some string from props'}
-//         />);
-//     getByText('some string from props');
-// });
-
-it('react test library', () => {
+it('mock store', () => {
     const { getByText } = render(
         <Provider store={mockStore}>
             <App>               
@@ -39,3 +22,14 @@ it('react test library', () => {
     getByText('some string form react props');
     getByText('petrov')
 });
+
+
+
+// it('react test library', () => {
+//     const { getByText } = render(
+//         <ListOfPersonsWithoutRedux
+//             persons={persons}
+//             someString={'some string from props'}
+//         />);
+//     getByText('some string from props');
+// });
