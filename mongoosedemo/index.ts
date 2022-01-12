0741f533beb9920609c8cf5c3df36c96fec5a998
sekhanov <sergey.khanov@akvelon.com>
2022-01-12 12:16:22 +0300

    mongoose demo

--- mongoosedemo/index.ts
+++ mongoosedemo/index.ts
@@ -2,15 +2,26 @@ import {Schema, connect, model, Document, Model} from 'mongoose';
 
 const db = connect('mongodb://localhost:27017/test', { useUnifiedTopology: true, useNewUrlParser: true } );
 
+interface IUser {
+    name: string;
+}
 
-const userSchema =  new Schema({
-    name: String,
+interface IUserModel extends Document, IUser {}
+
+const userSchema: Schema<IUserModel> =  new Schema({
+    name: {type: String, unique: true}
 })
 
- const UserModel = model('User', userSchema);
+ const UserModel: Model<IUserModel> = model('User', userSchema);
 
+ //one way
  const user = new UserModel({
      name: "ivan",
  })
+ user.save().catch(e => console.log(e));
 
- user.save();
+ //second way
+ const user2 = UserModel.create({name: "sergey"}, (e, u: IUserModel) => {
+     if(e) console.log(e);
+     console.log(u.name);
+ })
